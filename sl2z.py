ef71fde6bf8fd5632a33cad3e2e66d543573ae34
rikyuusima <equanz324@gmail.com>
2016-12-22 16:22:50 +0900

    fix 括弧

--- sl2z.py
+++ sl2z.py
@@ -5,7 +5,7 @@ def judge_sl2Z(matrix):
     # Define Determinant for ∈ SL(2, Z)
     SL2Z_DET = 1
 
-    if(int(la.det(matrix)) == SL2Z_DET):
+    if int(la.det(matrix)) == SL2Z_DET:
         return True
     else:
         raise ValueError("Input matrix is not element of SL(2, Z)")
